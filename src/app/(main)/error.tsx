9be674e0c23bd7123439c194a030b5b40cfaf387
Edlâneo Júlio <edlanioj@gmail.com>
2023-01-31 22:15:20 +0100

    update home error page

--- src/app/(main)/error.tsx
+++ src/app/(main)/error.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import clsx from 'clsx';
+import { WarningCircle } from 'phosphor-react';
 import { useEffect } from 'react';
 
 export default function Error({ error, reset }: any) {
@@ -7,8 +9,23 @@ export default function Error({ error, reset }: any) {
     console.log({ error });
   }, [error]);
   return (
-    <div className="flex h-full justify-center items-center text-center">
-      <strong>{error?.message}</strong>
+    <div
+      className={clsx(
+        'absolute top-0 w-full h-full',
+        ' flex flex-col gap-4 items-center justify-center text-center',
+        'text-gray-100 px-4'
+      )}
+    >
+      <WarningCircle className="text-6xl" weight="bold" />
+      <span className="text-3xl font-bold">
+        Ocorreu um problema ao carregar essa pagina
+      </span>
+      <button
+        className="p-2 px-4 uppercase rounded-full bg-gray-300 font-semibold text-white text-sm"
+        onClick={() => reset()}
+      >
+        Recarregar
+      </button>
     </div>
   );
 }
--- src/app/(main)/loading.tsx
+++ src/app/(main)/loading.tsx
@@ -3,7 +3,7 @@
 export default function Loading() {
   return (
     <div className="absolute top-0 flex w-full h-full items-center justify-center text-gray-100">
-      <div className="flex gap-3">
+      <div className="flex gap-2">
         <div className="w-2 h-2 bg-gray-100 rounded-full animate-left-bobble" />
         <div className="w-2 h-2 bg-gray-100 rounded-full animate-center-bobble animation-delay-200" />
         <div className="w-2 h-2 bg-gray-100 rounded-full animate-right-bobble animation-delay-400" />
