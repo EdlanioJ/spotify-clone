50045dd03d896189fff88d1b5e31eea6afeccba3
Edlâneo Júlio <edlanioj@gmail.com>
2023-01-31 07:36:33 +0100

    refactor loading page

--- src/app/(main)/loading.tsx
+++ src/app/(main)/loading.tsx
@@ -2,8 +2,12 @@
 
 export default function Loading() {
   return (
-    <div className="flex w-full h-full items-center justify-center">
-      <h1>Loading</h1>
+    <div className="absolute top-0 flex w-full h-full items-center justify-center text-gray-100">
+      <div className="flex gap-3">
+        <div className="w-2 h-2 bg-gray-100 rounded-full animate-left-bobble" />
+        <div className="w-2 h-2 bg-gray-100 rounded-full animate-center-bobble animation-delay-200" />
+        <div className="w-2 h-2 bg-gray-100 rounded-full animate-right-bobble animation-delay-400" />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,48 @@ module.exports = {
           900: '#191414',
         },
       },
+      animation: {
+        'left-bobble': 'left-bobble 1200ms ease-in-out infinite',
+        'right-bobble': 'right-bobble 1200ms ease-in-out infinite',
+        'center-bobble': 'center-bobble 1200ms ease-in-out infinite',
+      },
+      keyframes: {
+        'left-bobble': {
+          '0%, 100%': {
+            transform: 'translateX(0px) scale(1)',
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateX(-10px) scale(0.75)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+        'center-bobble': {
+          '0%, 100%': {
+            transform: 'scale(1)',
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'scale(0.75)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+        'right-bobble': {
+          '0%, 100%': {
+            transform: 'translateX(0px) scale(1)',
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateX(10px) scale(0.75)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+      },
     },
   },
-  plugins: [require('@tailwindcss/line-clamp'), require('tailwind-scrollbar')],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('tailwind-scrollbar'),
+    require('./plugins/animationDelay'),
+  ],
 };
