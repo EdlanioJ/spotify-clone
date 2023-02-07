ea3c28118b5cd3bbde08d208294dd1189baa96da
Edlâneo Júlio <edlanioj@gmail.com>
2023-02-07 06:01:15 +0100

    delete unused files

--- src/app/(main)/search/template.tsx
+++
@@ -1,6 +0,0 @@
-import { useResetColorEffect } from '@context/ColorContext';
-
-export default function Template({ children }: { children: React.ReactNode }) {
-  useResetColorEffect();
-  return <>{children}</>;
-}
--- src/components/InputRange.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useId } from 'react';
-
-export function InputRange({ ...rest }) {
-  const id = useId();
-
-  return <input type="range" id={id} {...rest} />;
-}
