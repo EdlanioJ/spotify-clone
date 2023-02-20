e624331f74f5b38d374c61176486a017d424e991
Edlâneo Júlio <edlanioj@gmail.com>
2023-02-20 02:17:10 +0100

    add fetch categories in search page

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,6 +1,21 @@
-export default function SearchPage() {
+import { authOptions } from '@api/auth/[...nextauth]';
+import { spotifyApi } from '@libs/spotify';
+import { getServerSession } from 'next-auth';
+
+async function fetchData() {
+  const session = await getServerSession(authOptions);
+  const accessToken = session?.accessToken as string;
+  spotifyApi.setAccessToken(accessToken);
+  const { body: categories } = await spotifyApi.getCategories({
+    limit: 50,
+  });
+
+  return categories;
+}
+export default async function SearchPage() {
+  const categories = await fetchData();
   return (
-    <div className="flex h-full items-center justify-center">
+    <div className="">
       <h1>Pagina de pesquisa</h1>
     </div>
   );
