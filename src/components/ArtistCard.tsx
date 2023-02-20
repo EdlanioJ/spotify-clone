731ef6a27254e445a2895e2ed1889bc80a43bd4b
Edlâneo Júlio <edlanioj@gmail.com>
2023-02-20 02:14:38 +0100

    update artist card component

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { Slot } from '@radix-ui/react-slot';
 import Image from 'next/image';
-import { PlayButton } from './Card';
+import PlayButton from './PlayButton';
 
 type Props = {
   artist: SpotifyApi.ArtistObjectFull;
@@ -12,15 +13,20 @@ export default function ArtistCard({ artist }: Props) {
     <div className="flex flex-col p-3 pb-5 rounded-lg hover:bg-zinc-800 delay-100 duration-100 shadow-xl bg-zinc-800/60 relative group transition-colors select-none overflow-hidden">
       <div className="relative w-full">
         <div className=" w-full">
-          <Image
-            alt={artist.name}
-            src={artist.images[2].url}
-            width={artist.images[2].width}
-            height={artist.images[2].width}
-            className="w-36 h-36 object-cover shadow-[0px_8px_24px_rgba(0,0,0,0.5)] rounded-full"
-          />
+          {!artist.images[0] ? (
+            <div></div>
+          ) : (
+            <Image
+              alt={artist.name}
+              src={artist.images[0].url}
+              width={artist.images[0].width}
+              height={artist.images[0].width}
+              className="w-36 h-36 object-cover shadow-[0px_8px_24px_rgba(0,0,0,0.5)] rounded-full"
+            />
+          )}
         </div>
-        <PlayButton />
+
+        <PlayButton variant="card" />
       </div>
       <strong className="text-sm text-gray-200 font-bold truncate mt-2 mb-2">
         {artist.name}
