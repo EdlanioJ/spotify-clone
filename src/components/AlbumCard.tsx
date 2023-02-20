256a11645d5360de3688cfadef5d954c9888a405
Edlâneo Júlio <edlanioj@gmail.com>
2023-02-20 02:14:12 +0100

    update album card component

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Image from 'next/image';
 import Card from './Card';
+import PlayButton from './PlayButton';
 
 type Props = {
   album: SpotifyApi.AlbumObjectSimplified;
@@ -10,7 +10,11 @@ type Props = {
 export default function AlbumCard({ album }: Props) {
   return (
     <Card>
-      <Card.Image alt={album.name} src={album.images[1].url} />
+      <Card.Image
+        alt={album.name}
+        src={album.images[1].url}
+        playBtn={<PlayButton variant="card" />}
+      />
       <Card.Title>{album.name}</Card.Title>
       <Card.Description>{album.artists[0].name}</Card.Description>
     </Card>
