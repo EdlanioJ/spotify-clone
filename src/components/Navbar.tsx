fd4c9bc995a4d6678f8b8450192e634fde8588e3
Edlâneo Júlio <edlanioj@gmail.com>
2023-02-20 01:56:10 +0100

    add search input to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,73 @@
 'use client';
 
 import clsx from 'clsx';
-import { ButtonHTMLAttributes, useCallback } from 'react';
-import { CaretLeft } from 'phosphor-react';
+import {
+  ButtonHTMLAttributes,
+  ChangeEvent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { CaretLeft, MagnifyingGlass } from 'phosphor-react';
+import debounce from 'lodash/debounce';
 import { Slot } from '@radix-ui/react-slot';
-import { useRouter } from 'next/navigation';
-import { Session } from 'next-auth';
+import { useRouter, usePathname } from 'next/navigation';
+import type { Session } from 'next-auth';
 
 import { useScroll } from '@context/ScrollContext';
 import { useColor } from '@context/ColorContext';
 
-import Menu from '@components/Menu';
+import Menu from './Menu';
 
 type Props = {
   user?: Session['user'];
 };
 
 export default function Navbar({ user }: Props) {
-  const { scrollY } = useScroll();
+  const [query, setQuery] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const pathname = usePathname();
   const router = useRouter();
+  const { scrollY } = useScroll();
   const { color } = useColor();
+
+  useEffect(() => {
+    if (pathname === '/search') {
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    }
+  }, [pathname]);
+
+  useEffect(() => {
+    if (query.trim().length !== 0) {
+      router.push(`/search/${query}`);
+    }
+  }, [query, router]);
+
+  useEffect(() => {
+    if (
+      pathname &&
+      pathname.includes('/search/') &&
+      query.trim().length === 0
+    ) {
+      router.back();
+    }
+  }, [pathname, router, query]);
+
+  function searchInputHandler(event: ChangeEvent<HTMLInputElement>) {
+    setQuery(event.target.value);
+  }
+
+  const onChangeSearchInput = useMemo(
+    () => debounce(searchInputHandler, 150),
+    []
+  );
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const onChange = useCallback(debounce(searchInputHandler, 150), []);
+
   const handleGoBack = useCallback(() => {
     router.back();
   }, [router]);
@@ -37,10 +85,23 @@ export default function Navbar({ user }: Props) {
         {scrollY > 90 && <div className="bg-black/60 h-full" />}
       </div>
 
-      <div className="relative">
+      <div className="relative flex gap-2">
         <IconButton onClick={handleGoBack} disabled>
           <CaretLeft weight="bold" />
         </IconButton>
+        {pathname?.includes('/search') && (
+          <div className="flex items-center justify-center h-7 w-72 pl-2 bg-white rounded-full overflow-hidden">
+            <MagnifyingGlass className="text-gray-700" weight="bold" />
+            <input
+              className="outline-none h-full w-full pl-1 text-sm font-medium text-gray-700"
+              type="text"
+              placeholder="O que queres ouvir?"
+              onChange={onChange}
+              defaultValue={query}
+              ref={inputRef}
+            />
+          </div>
+        )}
       </div>
 
       <div className="relative">
