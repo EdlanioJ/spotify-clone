cdb7cd33fa508e6b1de30d6713b99b4db32df6b9
Edlâneo Júlio <edlanioj@gmail.com>
2023-02-20 02:08:25 +0100

    update card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,18 @@
 'use client';
 
 import React from 'react';
-import { Play } from 'phosphor-react';
+import { MusicNotes, Play } from 'phosphor-react';
 import NextImage, { ImageProps as NextImageProps } from 'next/image';
 import clsx from 'clsx';
+import { Slot } from '@radix-ui/react-slot';
+import PlayButton from './PlayButton';
 
 type CardProps = {
   children?: React.ReactNode;
 };
 function Card({ children }: CardProps) {
   return (
-    <div className="flex flex-1 flex-col p-3 pb-5 rounded-lg hover:bg-zinc-800 delay-100 duration-100 shadow-xl bg-zinc-800/60 overflow-hidden relative group transition-colors select-none">
+    <div className="flex flex-1 flex-col h-full p-3 rounded-lg hover:bg-zinc-800 delay-100 duration-100 shadow-xl bg-zinc-800/60 overflow-hidden relative group transition-colors select-none">
       {children}
     </div>
   );
@@ -18,20 +20,43 @@ function Card({ children }: CardProps) {
 
 type ImageProps = {
   variant?: 'cover' | 'avatar';
+  playBtn: React.ReactNode;
 } & NextImageProps;
-function Image({ variant = 'cover', ...rest }: ImageProps) {
+function Image({ variant = 'cover', playBtn, ...rest }: ImageProps) {
   return (
-    <div className="relative object-contain w-full">
+    <div className="relative w-full">
       <NextImage
-        className={clsx('w-full shadow-[0px_8px_24px_rgba(0,0,0,0.5)]', {
-          'rounded ': variant === 'cover',
-          'rounded-full': variant === 'avatar',
-        })}
+        className={clsx(
+          'w-full object-cover h-full shadow-[0px_8px_24px_rgba(0,0,0,0.5)]',
+          {
+            'rounded ': variant === 'cover',
+            'rounded-full': variant === 'avatar',
+          }
+        )}
         width={100}
         height={100}
         {...rest}
       />
-      <PlayButton />
+      {playBtn}
+    </div>
+  );
+}
+
+type CoverProps = {
+  variant?: 'playlist' | 'artist';
+  playBtn: React.ReactNode;
+};
+function Cover({ variant = 'playlist', playBtn }: CoverProps) {
+  return (
+    <div
+      className={clsx(
+        'relative w-full h-full rounded flex items-center justify-center bg-zinc-900 shadow-[0px_8px_24px_rgba(0,0,0,0.5)]',
+        'text-gray-200 py-4 text-3xl'
+      )}
+    >
+      {variant === 'playlist' && <MusicNotes className="" weight="bold" />}
+
+      {playBtn}
     </div>
   );
 }
@@ -44,7 +69,7 @@ function Title({ children, className }: TitleProps) {
   return (
     <h2
       className={clsx(
-        'text-sm text-gray-200 font-bold truncate mt-3 mb-2',
+        'text-sm text-gray-200 font-bold truncate mt-2 mb-1',
         className
       )}
     >
@@ -58,23 +83,15 @@ type DescriptionProps = {
 };
 function Description({ children }: DescriptionProps) {
   return (
-    <span className="font-semibold line-clamp-2 text-xs text-gray-400 capitalize">
+    <span className="font-medium line-clamp-2 text-xs text-gray-400 capitalize">
       {children}
     </span>
   );
 }
 
-export function PlayButton() {
-  return (
-    <button className="w-11 h-11 rounded-full shadow-md bg-green-600 absolute top-[60%] right-[6%] grid place-content-center hover:scale-110 translate-y-2 drop-shadow-sm transition-all opacity-0 group-hover:opacity-100 group-hover:translate-y-0">
-      <Play className="text-black text-2xl" weight="fill" />
-    </button>
-  );
-}
-
 Card.Title = Title;
 Card.Image = Image;
+Card.Cover = Cover;
 Card.Description = Description;
-Card.PlayButton = PlayButton;
 
 export default Card;
